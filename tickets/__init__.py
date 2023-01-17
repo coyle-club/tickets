8d9c8027e0f0c13176bcccd53aaa7c56d520ff16
Tom Petr <tom@r2c.dev>
2023-01-17 08:46:37 -0500

    Update __init__.py

--- tickets/__init__.py
+++ tickets/__init__.py
@@ -32,6 +32,9 @@ if True is True:
     pass
 
 
+if True is True:
+    pass
+
 app = Quart("tickets")
 app.config.from_mapping(DB="/var/lib/tickets/tickets.db")
 app.config.from_prefixed_env("TICKETS_")
